508eb0245e2393055e234bce4ef6a15e6d71242d
Chris Berry-Porter <berryporter.chris@gmail.com>
2015-05-11 21:47:54 +1000

    Updated usage string to include channel flag

--- src/wisn.c
+++ src/wisn.c
@@ -14,6 +14,7 @@ char *wifiInterface;    //Name of wifi interface
 const char *usage = "Usage: wisn node_num wifi_interface [OPTIONS]\n\n"
                     "-b broker\tMQTT broker to use\n"
                     "-p port\t\tMQTT port to use\n"
+                    "-c channel\tOnly listen on the specified wireless channel"
                     "-v\t\twisn version\n";         //Usage String
 
 pcap_t *pcapHandle;             //Pcap handle for the wifi interface to listen on
--- src/wisn_version.h
+++ src/wisn_version.h
@@ -1,3 +1,3 @@
 #ifndef WISN_VERSION
-#define WISN_VERSION "0.0.2"
+#define WISN_VERSION "0.0.3"
 #endif
